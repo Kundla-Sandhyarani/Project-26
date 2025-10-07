c6b7c2f0cc182b44d4893f81375c5133488664ff
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-10-07 15:09:09 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,5 @@
 echo "Running install_dependencies.sh"
 echo "PATH: $PATH"
 which npm || echo "npm not found"
-npm install || echo "npm install failed"
+sudo npm install || echo "npm install failed"
 
