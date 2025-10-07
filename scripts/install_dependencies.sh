7117db2bdb665943fc07c1babbae7dc2416c6cb3
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-10-07 14:35:58 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Install Node.js and npm
+curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+yum install -y nodejs
+
+# Navigate to app directory and install dependencies
 cd /home/ec2-user/node-app
 npm install
