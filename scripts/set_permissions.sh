bb8774901e1193f5e51e52ed3be82b79eb5a791c
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-10-07 15:08:44 +0530

    Update set_permissions.sh

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ensure ownership and permissions
-chown -R ec2-user:ec2-user /home/ec2-user/node-app
-chmod +x /home/ec2-user/node-app/scripts/*.sh
+sudo chown -R ec2-user:ec2-user /home/ec2-user/node-app
+sudo chmod +x /home/ec2-user/node-app/scripts/*.sh
