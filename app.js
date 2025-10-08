289c1b262eb823db9fda9b7a5040af10d33c6f04
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-10-08 15:16:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
-  res.end('Hi,Hello from Node.js on EC2!');
+  res.end('Hello from Node.js on EC2!');
 });
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
